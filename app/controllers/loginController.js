332471ac5d0a6588d79ad1822a2bf7da636bb4ef
Carolina-Melgarejo <cmelgarejo38@gmail.com>
2023-06-28 11:37:07 -0400

    prueba

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,40 +1,23 @@
 
-var jwt = require('jsonwebtoken');
-var User = require('../models/userModel'); 
-var config = require('../config/db');  
+const router = require("express").Router();
 
-exports.login = function (req, res) {
-    User.findOne({
-        username: req.body.email
-    }, function (err, user) {
+const apiRoutes = require("./api");
+const indexRoutes = require("./index-routes");
+const loginRoutes = require("./login-routes");
+const postRoutes = require("./post-routes");
+const signupRoutes = require("./signup-routes");
+const logoutRoutes = require("./logout-routes");
+const dashboardRoutes = require("./dashboard-routes");
+const editRoutes = require("./edit-routes");
 
-        if (err) {
-            res.json({success: false, statusCode: 500, errorMessage: err});
-        }
 
-        if (!user) {
-            res.json({success: false, statusCode: 403, errorMessage: 'Usuario No encontraddo'});
-        } else if (user) {
+router.use("/", indexRoutes);
+router.use("/api", apiRoutes);
+router.use("/login", loginRoutes);
+router.use("/post", postRoutes);
+router.use("/signup", signupRoutes);
+router.use("/logout", logoutRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/edit", editRoutes);
 
-            
-            if (!user.comparePassword(req.body.password)) {
-                res.json({success: false, statusCode: 403, errorMessage: 'Contraseña incorrecta'});
-            } else {
-
-        
-                var token = jwt.sign(user, config.secret, {
-                    expiresInMinutes: 1440 // expires in 24 hours
-                });
-
-                res.json({
-                    success: true,
-                    statusCode: 200,
-                    message: 'Usted inicio sesion con exito',
-                    token: token
-                });
-            }
-
-        }
-
-    });
-};
+module.exports = router;
