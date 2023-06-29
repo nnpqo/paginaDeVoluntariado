18ef5fe5b5d2a208cbe8f4e8483015ed63b567c1
nnpqo <yaninapaolaquispeorellana@gmail.com>
2023-06-29 19:53:15 -0400

    cambios en login

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,35 +1,37 @@
-
 const express = require("express");
 const router = express.Router();
-const loginModel = require("../models/loginModel.js");
+const loginModel = require("../models/userModel.js");
 
 router.post("/create", async (req, res) => {
-    const { codigoVoluntario, nombre, contrasenia } = req.body;
-  
-    try {
-      const result = await usuarioModel.crearUsuario(codigoVoluntario, nombre, contrasenia);
-      res.send("Usuario creado con éxito");
-    } catch (err) {
-      console.error("Error al crear el usuario: ", err);
-      res.status(500).send("Error al crear el usuario");
+  const { codigoVoluntario, nombre, contrasenia } = req.body;
+
+  try {
+    const result = await usuarioModel.crearUsuario(
+      codigoVoluntario,
+      nombre,
+      contrasenia
+    );
+    res.send("Usuario creado con éxito");
+  } catch (err) {
+    console.error("Error al crear el usuario: ", err);
+    res.status(500).send("Error al crear el usuario");
+  }
+});
+
+router.post("/getUsuarioPorCodigo/:codigo/:contrasenia", async (req, res) => {
+  const codigo = req.params.codigo;
+  const contrasenia = req.params.contrasenia;
+  loginModel.getUsuarioLogin(codigo, contrasenia).then((usuario) => {
+    if (usuario[0]) {
+      res.json({login: true});
+    } else {
+      res.json({login: false});
     }
   });
-  
-  router.get("/getUsuarioPorCodigo/:codigo", async (req, res) => {
-    const { codigo } = req.params;
-  
-    try {
-      const usuario = await loginModel.obtenerVoluntarioPorCodigo(codigo);
-      res.json(usuario);
-    } catch (err) {
-      console.error("Error al obtener el usuario:", err);
-      res.status(500).send("Error al obtener el usuario");
-    }
-  });
-  
+});
+
 router.get("/", (req, res) => {
   res.render("login");
 });
-
 
 module.exports = router;
--- app/controllers/registerController.js
+++ app/controllers/registerController.js
@@ -1,36 +1,19 @@
 
 const express = require("express");
 const router = express.Router();
-const registerModel = require("../models/registerModel.js");
+const userModel = require("../models/userModel.js");
 
 
 router.post("/create", async (req, res) => {
     const { codigoVoluntario, nombre, contrasenia } = req.body;
   
     try {
-      const result = await registerModel.crearVoluntario(codigoVoluntario, nombre, contrasenia);
+      const result = await userModel.crearVoluntario(codigoVoluntario, nombre, contrasenia);
       res.send("Usuario creado con éxito");
     } catch (err) {
       console.error("Error al crear el usuario: ", err);
       res.status(500).send("Error al crear el usuario");
     }
-  });
-  
-  router.get("/getUsuarioPorCodigo/:codigo", async (req, res) => {
-    const { codigo } = req.params;
-  
-    try {
-      const usuario = await registerModel.obtenerVoluntarioPorCodigo(codigo);
-      res.json(usuario);
-    } catch (err) {
-      console.error("Error al obtener el usuario:", err);
-      res.status(500).send("Error al obtener el usuario");
-    }
-  });
-  
-router.get("/", (req, res) => {
-  res.render("register");
 });
-
 
 module.exports = router;
