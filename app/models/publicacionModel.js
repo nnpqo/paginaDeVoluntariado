2a5b99e41c5cc4a4ba0ebbbc041860edf5c92185
nnpqo <yaninapaolaquispeorellana@gmail.com>
2023-06-28 21:29:53 -0400

    registro con desplegable

--- app/models/publicacionModel.js
+++ app/models/publicacionModel.js
@@ -3,13 +3,13 @@ const db = require("../../config/db.js");
 const crearPublicacion = (nombre, descripcion, tipoPublicacion, fechaInicio, horaActividad, fechaCierre, ubicacionA, cantidadVoluntarios) => {
   return new Promise((resolve, reject) => {
     const selectDbQuery = `USE database_app;`;
-    const insertQuery = "INSERT INTO `publicaciones`(`nombre_publicacion`, `descripcion`, `tipo_publicacion`, `fecha_inicio`, `hora_actividad`, `fecha_cierre`, `ubicacion_actividad`, `cantidad_voluntarios`) VALUES (?,?,?,?,?,?,?,?)"
+    const insertQuery = "INSERT INTO `publicaciones`(`nombre_publicacion`, `descripcion`, `tipo_publicacion`, `cantidad_voluntarios`) VALUES (?,?,?,?)"
   
     db.query(selectDbQuery, (err) => {
       if (err) {
         reject(err);
       } else {
-        db.query(insertQuery, [nombre, descripcion, tipoPublicacion, fechaInicio, horaActividad, fechaCierre, ubicacionA, cantidadVoluntarios], (err, result) => {
+        db.query(insertQuery, [nombre, descripcion, tipoPublicacion, cantidadVoluntarios], (err, result) => {
           if (err) {
             reject(err);
           } else {
